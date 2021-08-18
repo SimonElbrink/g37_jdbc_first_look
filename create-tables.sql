91736787f40b245f1b2b3ac2fc948c2f257d988e
Simon Elbrink <simon.elbrink@gmail.com>
2021-08-18 14:35:52 +0200

    Added previous days progress as SQL FILES.

---
+++ create-tables.sql
@@ -0,0 +1,67 @@
+-- CREATE DB
+
+CREATE SCHEMA library;
+
+
+-- ACCESS DB
+USE library;
+
+
+
+-- DETAILS TABLE
+
+
+CREATE TABLE details (
+                         details_id INT NOT NULL AUTO_INCREMENT,
+                         email VARCHAR(50) NOT NULL UNIQUE,
+                         first_name VARCHAR(50) NOT NULL,
+                         last_name VARCHAR(50) NOT NULL,
+                         birth_date DATE NOT NULL,
+                         app_user_id INT,
+                         PRIMARY KEY (details_id)
+);
+
+
+-- APPUSER TABLE
+
+
+CREATE TABLE app_user (
+                          app_user_id INT NOT NULL AUTO_INCREMENT,
+                          user_name VARCHAR(50) NOT NULL UNIQUE,
+                          pass_word VARCHAR(50) NOT NULL,
+                          reg_date DATE,
+                          PRIMARY KEY (app_user_id)
+);
+
+CREATE TABLE book (
+                      book_id INT NOT NULL AUTO_INCREMENT,
+                      isbn VARCHAR(50) NOT NULL UNIQUE,
+                      title VARCHAR(50) NOT NULL,
+                      max_loan_days INT NOT NULL,
+                      PRIMARY KEY (book_id)
+);
+
+CREATE TABLE book_loan (
+                           loan_id INT NOT NULL AUTO_INCREMENT,
+                           loan_date DATE NOT NULL,
+                           due_date DATE,
+                           returned BOOLEAN,
+                           borrower_id INT,
+                           book_id INT,
+                           PRIMARY KEY (loan_id)
+);
+
+
+-- SETTING FOREIGN KEY etc.
+
+ALTER TABLE details
+    ADD CONSTRAINT FK_DETAILS_APPUSER FOREIGN KEY (app_user_id)
+        REFERENCES app_user(app_user_id);
+
+ALTER TABLE book_loan
+    ADD CONSTRAINT FK_BOOKLOAN_APPUSER
+        FOREIGN KEY (borrower_id) REFERENCES app_user(app_user_id);
+
+ALTER TABLE book_loan
+    ADD CONSTRAINT FK_BOOKLOAN_BOOK
+        FOREIGN KEY (book_id) REFERENCES book(book_id);
---
+++ crud-operations.sql
@@ -0,0 +1,33 @@
+USE library;
+
+--  CRUD
+-- CREATE
+-- SELECT
+-- UPDATE
+-- DELETE
+
+-- deleting one by one. (Removing the relationship before)
+DELETE FROM details WHERE app_user_id = 3;
+DELETE FROM app_user WHERE app_user_id = 3;
+
+-- clear the Tables
+TRUNCATE TABLE details;
+TRUNCATE TABLE app_user;
+
+-- Create
+INSERT INTO app_user (app_user_id, username, password, reg_date)
+VALUES (1, 'simonelbrink', '1234', '2020-01-01');
+
+INSERT INTO app_user (username, password, reg_date)
+VALUES ('eriksvensson', '56789', '2019-01-01');
+
+-- Let's update the password of eriksvensson
+UPDATE app_user SET password = '65876' WHERE app_user_id = 6;
+
+-- Find all
+SELECT * FROM app_user;
+
+-- find by username simonelbrink
+-- SYNTAX- SELECT [columns] FROM [tablename] WHERE [CONDITION]
+
+SELECT reg_date, password  FROM app_user WHERE username LIKE 's%';
